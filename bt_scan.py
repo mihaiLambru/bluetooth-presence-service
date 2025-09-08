965bda273119b946a204ecc3bfc9953b4587c760
Mihai Lambru <mihailambru99@gmail.com>
2025-09-08 18:52:09 +0300

    initial commit

---
+++ bt_scan.py
@@ -0,0 +1,41 @@
+import asyncio, json, time
+from bleak import BleakClient, BleakScanner
+
+async def scan_device(address: str, timeout: int):
+	"""Scan for a single device by address"""
+	try:
+		# timeout 10 minutes
+		device = await BleakScanner().find_device_by_address(address, timeout)
+		print(f"Found device {device.name} for address {address}")
+
+		return {"address": address, "device": device, "found": device is not None}
+	except Exception as e:
+		print(f"Error scanning device {address}: {e}")
+		return {"address": address, "device": None, "found": False, "error": str(e)}
+
+async def scan_devices(known_devices: list[str], timeout: int):
+	print(f"Scanning for {len(known_devices)} devices with timeout {timeout} seconds")
+	# Mark scanning active
+	start_time = time.time()
+	result = {"found_devices": [], "run_time": 0}
+
+	tasks = []
+
+	# connect to known devices
+	for address in known_devices:
+		tasks.append(scan_device(address, timeout))
+
+	results = await asyncio.gather(*tasks)
+	result["found_devices"] = list(map(lambda x: x["address"], results))
+
+	end_time = time.time()
+	result["run_time"] = end_time - start_time
+	print(f"Result: {result}")
+	# Mark scanning done
+	print(json.dumps(result))
+
+if __name__ == "__main__":
+	asyncio.run(scan_devices(["AC:DF:A1:C3:80:E3"], 30))
+
+
+
